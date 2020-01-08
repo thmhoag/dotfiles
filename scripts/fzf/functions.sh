3658e4209f8db52640a2420cfc15b1d51f961b07
Thomas Hoag <thmhoag@gmail.com>
2020-01-08 18:20:55 -0500

    Add some fzf scripts

---
+++ scripts/fzf/functions.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# fd - cd to selected directory
+fd() {
+  DIR=`find * -maxdepth 5 -type d -print 2> /dev/null | fzf-tmux` \
+    && cd "$DIR"
+}
+
+# using ripgrep combined with preview
+# find-in-file - usage: fif <searchTerm>
+fif() {
+  if [ ! "$#" -gt 0 ]; then echo "Need a string to search for!"; return 1; fi
+  rg --files-with-matches --no-messages "$1" | fzf --preview "highlight -O ansi -l {} 2> /dev/null | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 '$1' || rg --ignore-case --pretty --context 10 '$1' {}"
+}
+
+# fkill - kill process
+fkill() {
+  local pid
+  pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+
+  if [ "x$pid" != "x" ]
+  then
+    echo $pid | xargs kill -${1:-9}
+  fi
+}
+
