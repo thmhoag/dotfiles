a6e0ff63e5c9c63c738a5335da85e612638f6ae1
Thomas Hoag <thmhoag@gmail.com>
2020-03-07 15:02:43 -0500

    Fixes for tmux install, remove dependency on wget

--- scripts/install/tmux.sh
+++ scripts/install/tmux.sh
@@ -2,14 +2,13 @@
 
 set -eo pipefail
 
-# Steps to build and install tmux from source on Ubuntu.
-# Takes < 25 seconds on EC2 env [even on a low-end config instance].
 VERSION=2.8
 
+apt-get update
 apt-get -y remove tmux
-apt-get -y install wget tar libevent-dev libncurses-dev
+apt-get -y install git automake build-essential pkg-config libevent-dev libncurses5-dev
 
-wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
+curl -LO https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
 
 tar xf tmux-${VERSION}.tar.gz
 rm -f tmux-${VERSION}.tar.gz
