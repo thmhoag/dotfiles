5a8f1c5e361818a8353155029101bc6968203f20
Thomas Hoag <thmhoag@gmail.com>
2020-06-13 18:22:51 -0400

    Update ripgrep to manually install the correct version

--- scripts/install/ripgrep.sh
+++ scripts/install/ripgrep.sh
@@ -2,6 +2,12 @@
 
 set -eo pipefail
 
-apt-get download ripgrep
+RIPGREP_VERSION="$(curl -s https://api.github.com/repos/BurntSushi/ripgrep/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')"
+
+echo -e "Installing ripgrep $RIPGREP_VERSION\n\n\n"
+
+curl -sLO https://github.com/BurntSushi/ripgrep/releases/download/${RIPGREP_VERSION}/ripgrep_${RIPGREP_VERSION}_amd64.deb
 
 sudo dpkg --force-overwrite -i ripgrep*.deb
+
+rm ripgrep*.deb
