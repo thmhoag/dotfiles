4405cec10a080a1c7c5d127d998a29e28fd98de9
Thomas Hoag <thmhoag@gmail.com>
2023-06-21 10:51:33 -0400

    Update k9s install script

--- scripts/install/k9s.sh
+++ scripts/install/k9s.sh
@@ -5,9 +5,9 @@ set -eo pipefail
 VERSION="$(curl -s https://api.github.com/repos/derailed/k9s/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')"
 echo -e "installing k9s ${VERSION}..."
 
-mkdir -p /tmp/k9s_Linux_x86_64
-curl -sL https://github.com/derailed/k9s/releases/download/${VERSION}/k9s_Linux_x86_64.tar.gz | tar xz -C /tmp/k9s_Linux_x86_64
-chmod a+rx /tmp/k9s_Linux_x86_64/k9s
+mkdir -p /tmp/k9s_Linux_amd64
+curl -sL https://github.com/derailed/k9s/releases/download/${VERSION}/k9s_Linux_amd64.tar.gz | tar xz -C /tmp/k9s_Linux_amd64
+chmod a+rx /tmp/k9s_Linux_amd64/k9s
 
-sudo mv /tmp/k9s_Linux_x86_64/k9s /usr/local/bin
-rm -rf /tmp/k9s_Linux_x86_64
+sudo mv /tmp/k9s_Linux_amd64/k9s /usr/local/bin
+rm -rf /tmp/k9s_Linux_amd64
