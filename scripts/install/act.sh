c4d95363346554562031350b4d658c5305979717
Thomas Hoag <thmhoag@gmail.com>
2023-09-22 15:11:49 -0400

    Update act install script

--- scripts/install/act.sh
+++ scripts/install/act.sh
@@ -2,4 +2,15 @@
 
 set -eo pipefail
 
-curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
+VERSION=$(curl -L -s https://api.github.com/repos/nektos/act/releases/latest | grep tag_name | sed "s/ *\"tag_name\": *\"\\(.*\\)\",*/\\1/" | grep -oE "[0-9]+[.][0-9]+[.][0-9]+")
+
+echo -e "installing act ${VERSION}..."
+
+mkdir -p /tmp/act_Linux_x86_64
+curl -sL https://github.com/nektos/act/releases/download/v${VERSION}/act_Linux_x86_64.tar.gz | tar xz -C /tmp/act_Linux_x86_64
+chmod a+rx /tmp/act_Linux_x86_64/act
+
+sudo mv /tmp/act_Linux_x86_64/act /usr/local/bin
+rm -rf /tmp/act_Linux_x86_64
+
+echo "act installed"
